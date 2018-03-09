6e8eec851f974f82cf153ec7c2cf3eca279a01b8
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2018-03-09 15:01:17 +0100

    Set structure for repository.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='keras-maskrcnn',
+    version='0.0.1',
+    url='https://github.com/fizyr/keras-maskrcnn',
+    author='Hans Gaiser',
+    author_email='h.gaiser@fizyr.com',
+    maintainer='Hans Gaiser',
+    maintainer_email='h.gaiser@fizyr.com',
+    packages=setuptools.find_packages(),
+    install_requires=['keras', 'keras-retinanet'],
+    #entry_points = {
+    #    'console_scripts': [
+    #        'maskrcnn-train=keras_maskrcnn.bin.train:main',
+    #    ],
+    #}
+)
