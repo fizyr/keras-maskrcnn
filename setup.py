25805f2f4eab04e0f02184bea5b8f448842990a1
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2018-05-15 11:43:44 +0200

    Add description.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name='keras-maskrcnn',
     version='0.0.1',
+    description='Keras implementation of MaskRCNN instance aware segmentation.',
     url='https://github.com/fizyr/keras-maskrcnn',
     author='Hans Gaiser',
     author_email='h.gaiser@fizyr.com',
@@ -10,9 +11,9 @@ setuptools.setup(
     maintainer_email='h.gaiser@fizyr.com',
     packages=setuptools.find_packages(),
     install_requires=['keras', 'keras-retinanet'],
-    #entry_points = {
-    #    'console_scripts': [
-    #        'maskrcnn-train=keras_maskrcnn.bin.train:main',
-    #    ],
-    #}
+    entry_points = {
+        'console_scripts': [
+            'maskrcnn-train=keras_maskrcnn.bin.train:main',
+        ],
+    }
 )
