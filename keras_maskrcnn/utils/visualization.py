bd5805bc9fe67d4618fae3d01816c6573bf81de3
Hans Gaiser <j.c.gaiser@delftrobotics.com>
2018-05-16 16:22:05 +0200

    Add draw_masks visualization utility.

--- keras_maskrcnn/utils/visualization.py
+++ keras_maskrcnn/utils/visualization.py
@@ -10,7 +10,7 @@ def draw_mask(image, box, mask, color=None, binarize_threshold=0.5):
         image              : Three dimensional image to draw on.
         box                : Vector of at least 4 values (x1, y1, x2, y2) representing a box in the image.
         mask               : A 2D float mask which will be reshaped to the size of the box, binarized and drawn over the image.
-        color              : Color to draw the mask with.
+        color              : Color to draw the mask with. If the box has 5 values, the last value is assumed to be the label and used to construct a default color.
         binarize_threshold : Threshold used for binarizing the mask.
     """
     if color is None:
@@ -47,3 +47,17 @@ def draw_mask(image, box, mask, color=None, binarize_threshold=0.5):
     # draw the border
     indices = np.where(border != [0, 0, 0])
     image[indices[0], indices[1], :] = 0.2 * image[indices[0], indices[1], :] + 0.8 * border[indices[0], indices[1], :]
+
+
+def draw_masks(image, boxes, masks, color=None, binarize_threshold=0.5):
+    """ Draws a list of masks given a list of boxes.
+
+    Args
+        image              : Three dimensional image to draw on.
+        box                : Matrix of shape (N, >=4) (at least 4 values: (x1, y1, x2, y2)) representing boxes in the image.
+        mask               : Matrix of shape (N, H, W) of N masks of shape (H, W) which will be reshaped to the size of the corresponding box, binarized and drawn over the image.
+        color              : Single color or list of color to draw the masks with.
+        binarize_threshold : Threshold used for binarizing the masks.
+    """
+    for box, mask in zip(boxes, masks):
+        draw_mask(image, box, mask, color=color, binarize_threshold=binarize_threshold)
