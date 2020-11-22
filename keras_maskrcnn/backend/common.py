9a45d473692e5cce60706f60a986f22c283ea6a9
DataCrusade1999 <ashutosh.pandeyhlr007@gmail.com>
2020-11-22 17:14:55 +0530

    Minor Change

--- keras_maskrcnn/backend/common.py
+++ keras_maskrcnn/backend/common.py
@@ -14,11 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import keras.backend
+import tensorflow.keras.backend
 
 
 def log2(x):
-    return keras.backend.log(x) / keras.backend.log(keras.backend.cast(2.0, x.dtype))
+    return tensorflow.keras.backend.log(x) / tensorflow.keras.backend.log(tensorflow.keras.backend.cast(2.0, x.dtype))
 
 
 def overlap(a, b):
@@ -33,14 +33,14 @@ def overlap(a, b):
     """
     area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
 
-    iw = keras.backend.minimum(keras.backend.expand_dims(a[:, 2], axis=1), b[:, 2]) - keras.backend.maximum(keras.backend.expand_dims(a[:, 0], axis=1), b[:, 0])
-    ih = keras.backend.minimum(keras.backend.expand_dims(a[:, 3], axis=1), b[:, 3]) - keras.backend.maximum(keras.backend.expand_dims(a[:, 1], axis=1), b[:, 1])
+    iw = tensorflow.keras.backend.minimum(tensorflow.keras.backend.expand_dims(a[:, 2], axis=1), b[:, 2]) - tensorflow.keras.backend.maximum(tensorflow.keras.backend.expand_dims(a[:, 0], axis=1), b[:, 0])
+    ih = tensorflow.keras.backend.minimum(tensorflow.keras.backend.expand_dims(a[:, 3], axis=1), b[:, 3]) - tensorflow.keras.backend.maximum(tensorflow.keras.backend.expand_dims(a[:, 1], axis=1), b[:, 1])
 
-    iw = keras.backend.maximum(iw, 0)
-    ih = keras.backend.maximum(ih, 0)
+    iw = tensorflow.keras.backend.maximum(iw, 0)
+    ih = tensorflow.keras.backend.maximum(ih, 0)
 
-    ua = keras.backend.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
-    ua = keras.backend.maximum(ua, keras.backend.epsilon())
+    ua = tensorflow.keras.backend.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
+    ua = tensorflow.keras.backend.maximum(ua, tensorflow.keras.backend.epsilon())
 
     intersection = iw * ih
 
